8b4d1dccde75d8cda49afc5542c10c0cea1b611d
Melech Mizrachi <melechmizrachi@gmail.com>
2026-05-06 15:06:15 -0700

    [CI] Add GitHub Actions workflows and CI tool configs for TypeScript valkyrja.

---
+++ .github/ci/prettier/prettier.config.js
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+const config = {
+    semi         : true,
+    singleQuote  : true,
+    tabWidth     : 4,
+    trailingComma: 'all',
+    printWidth   : 120,
+};
+
+export default config;
