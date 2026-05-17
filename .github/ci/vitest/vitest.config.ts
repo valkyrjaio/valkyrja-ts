7229aea0159cb27711ba70c8865458e67dfd9632
Melech Mizrachi <melechmizrachi@gmail.com>
2026-05-16 23:38:18 -0700

    [CI] Fixing ci issues.

--- .github/ci/vitest/vitest.config.ts
+++ .github/ci/vitest/vitest.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
     test: {
-        include : ['../../../tests/**/*.test.ts'],
-        coverage: {
+        include         : ['../../../tests/**/*.test.ts'],
+        passWithNoTests : true,
+        coverage        : {
             provider: 'v8',
             reporter : ['text', 'lcov'],
         },
