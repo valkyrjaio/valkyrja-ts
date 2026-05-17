f42fb930de89548d367006d0c62781a94d1c2dcc
Melech Mizrachi <melechmizrachi@gmail.com>
2026-05-16 23:29:08 -0700

    [CI] Fixing ci issues.

--- .github/ci/eslint/eslint.config.js
+++ .github/ci/eslint/eslint.config.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
@@ -8,7 +9,7 @@ export default tseslint.config(
         languageOptions: {
             parserOptions: {
                 projectService: true,
-                tsconfigRootDir: import.meta.dirname,
+                tsconfigRootDir: path.resolve(import.meta.dirname, '../../../'),
             },
         },
     },
