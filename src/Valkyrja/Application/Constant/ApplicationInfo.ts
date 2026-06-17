3209cb316984d4172417ce7417d6bca189b96ed9
MelechMizrachi <1179171+MelechMizrachi@users.noreply.github.com>
2026-06-17 16:07:31 +0000

    [Release] Update ApplicationInfo for v26.0.0 release

--- src/Valkyrja/Application/Constant/ApplicationInfo.ts
+++ src/Valkyrja/Application/Constant/ApplicationInfo.ts
@@ -9,7 +9,7 @@
 
 export class ApplicationInfo {
     static readonly VERSION = '26.0.0' as const;
-    static readonly VERSION_BUILD_DATE_TIME = 'May 5 2026 16:12:18 MST' as const;
+    static readonly VERSION_BUILD_DATE_TIME = 'June 17 2026 09:06:29 MST' as const;
     static readonly ASCII = `
                  _ _               _
      /\\   /\\__ _| | | ___   _ _ __(_) __ _
